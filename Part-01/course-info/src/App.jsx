b30b841b096971683392fe9d52638e56f170caae
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-03 17:48:12 +0545

    `Refactored Content component to use parts array and map function`

--- Part-01/course-info/src/App.jsx
+++ Part-01/course-info/src/App.jsx
@@ -3,11 +3,13 @@ const Header = (props) => {
   return (<h1>{props.course}</h1>)
 }
 
-const Content = ({part1,part2,part3,exercises1,exercises2,exercises3}) => {
+const Content = ({parts}) => {
+  console.log(parts);
 return(<>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
+{parts.map(part =>{
+  <Part part={part.name} exercises={part.exercises} />
+})}
+        
       </>);
 }
 const Part = ({part, exercises}) => {
@@ -39,12 +41,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content
-        part1={parts[0].name}
-        exercises1={parts[0].exercises}
-        part2={parts[1].name}
-        exercises2={parts[1].exercises}
-        part3={parts[2].name}
-        exercises3={parts[2].exercises}
+        parts = {parts}
       />
       <Total
         exercises1={parts[0].exercises}
