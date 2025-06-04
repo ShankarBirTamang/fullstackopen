6c5d42035997386f7acb312a40611418752937f1
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-04 11:13:49 +0545

    `Refactor App component to use useState hook and remove external counter variable`

--- Part-01/counter-app/src/App.jsx
+++ Part-01/counter-app/src/App.jsx
@@ -1,5 +1,13 @@
-const App = (props) => {
-  return <div>Hello the count is {props.counter}</div>;
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(1);
+
+  setTimeout(() => {
+    setCount(count + 1);
+  }, 1000);
+
+  return <div>Hello the count is {count}</div>;
 };
 
 export default App;
--- Part-01/counter-app/src/index.jsx
+++ Part-01/counter-app/src/index.jsx
@@ -4,12 +4,6 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-let counter = 1;
-root.render(<App counter={counter}/>);
+root.render(<App/>);
 
-setInterval(() => {
-    counter += 1;
-    console.log(counter);
-    root.render(<App counter={counter}/>);
-}, 1000);
 
