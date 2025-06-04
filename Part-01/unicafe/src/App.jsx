abca871e7046f0244b8ffbbc47debccd64627545
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-04 17:14:32 +0545

    `Update dependencies in package.json and add Statistics component to App.jsx`

--- Part-01/unicafe/src/App.jsx
+++ Part-01/unicafe/src/App.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+const Statistics = ({good,setGood,bad,setBad,neutral,setNeutral}) => {
+
+  return (
+    <>
+    <h1>give feedback</h1>
+    <button onClick={() => setGood(good + 1)}>good</button>
+    <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+    <button onClick={() => setBad(bad + 1)}>bad</button>
+
+    <h1>statistics</h1>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {good + neutral + bad}</p>
+    <p>average {(good - bad) / (good + neutral + bad)}</p>
+    <p>positive {good / (good + neutral + bad) * 100} %</p>
+    </>
+  );
+}
+
 const App = () => {
-  return <div>Hello there</div>;
+
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return <div>
+
+    <Statistics 
+    good={good}  setGood={setGood} 
+    bad={bad} setBad={setBad}
+    neutral={neutral} setNeutral={setNeutral}
+    />
+  </div>;
 };
 
 export default App;
