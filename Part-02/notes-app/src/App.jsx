039667d93cbd9f94a0bb212863d54f9a837b6116
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-05 11:40:20 +0545

    `Update dependencies and App component to display notes list`

--- Part-02/notes-app/src/App.jsx
+++ Part-02/notes-app/src/App.jsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <div>Hello there</div>;
+const App = ({notes}) => {
+  return <>
+    <h1>Notes</h1>
+    <ul>
+      {notes.map(note => (
+        <li key={note.id}>
+          {note.content} <strong>{note.important ? "Important" : "Not Important"}</strong>
+        </li>
+      ))}
+    </ul>
+    <p>Note count: {notes.length}</p>
+  </>;
 };
 
 export default App;
