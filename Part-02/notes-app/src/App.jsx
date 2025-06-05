956c30a01d0db1c6bc41b6754b1123e754d13c6c
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-05 16:21:47 +0545

    `Added form to create new notes and updated state management`

--- Part-02/notes-app/src/App.jsx
+++ Part-02/notes-app/src/App.jsx
@@ -1,14 +1,40 @@
 import Notes from "./components/Notes";
+import { useState } from "react";
 
 const App = ({notes}) => {
+  const [myNotes,setMyNotes] = useState(notes);
+  const [newNote, setNewNote] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log("Adding a new note");
+    setMyNotes([
+      ...myNotes,
+      {
+        id: myNotes.length + 1,
+        content: newNote,
+        date: new Date().toISOString().split('T')[0],
+        important: Math.random() > 0.5 
+      }
+    ]);
+    setNewNote("");
+  }
+  const handleChange = (e)=>{
+    setNewNote(e.target.value);
+  }
+
   return <>
     <h1>Notes</h1>
     <ul>
-      {notes.map(note => (
+      {myNotes.map(note => (
         <Notes key={note.id} note={note} />
       ))}
     </ul>
-    <p>Note count: {notes.length}</p>
+    <p>Note count: {myNotes.length}</p>
+    <form onSubmit={handleSubmit}>
+      <input value={newNote} onChange={handleChange}/>
+      <button >Add Note</button>
+    </form>
   </>;
 };
 
