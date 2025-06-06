95d959dea6d262bb1f0e7bbde0037d44c2944e74
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-06 12:33:59 +0545

    `feat: updated package.json and App.jsx with new dependencies and functionality`

--- Part-02/phone-book/src/App.jsx
+++ Part-02/phone-book/src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , phone: '040-123456' },
-    { name: 'Ada Lovelace' , phone: '39-44-532352' },
-    { name: 'Dan Abramov' , phone: '12-43-234345' },
-    { name: 'Mary Poppendieck' , phone: '39-23-642'},
-    { name: 'John Doe' , phone: '123-456789' },
-    { name: 'Jane Smith' , phone: '987-654321' },
-    { name: 'Alice Johnson' , phone: '555-1234' },
-    { name: 'Bob Brown' , phone: '555-5678' },
-    { name: 'Charlie White' , phone: '555-8765' },
-    { name: 'Diana Green' , phone: '555-4321' }
-
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('');
   const [searchName, setSearchName] = useState('');
   const [newPhoneNo, setNewPhoneNo] = useState('');
+
+  useEffect(() => {
+    const myAxiosData = axios.get("http://localhost:3001/persons");
+    myAxiosData.then(response => {
+      console.log("Data fetched successfully:", response.data);
+      setPersons(response.data);
+    }).catch(error => {
+      console.error("Error fetching persons:", error);
+    });
+  },[]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
