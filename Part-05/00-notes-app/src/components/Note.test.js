aea38cad0469dbc1101ab5a18cc1219c17818c8e
ShankarBirTamang <sankar.jt19@gmail.com>
2025-07-22 13:54:02 +0545

    feat: add user interaction tests for Notes component; include new dependencies for testing

--- Part-05/00-notes-app/src/components/Note.test.js
+++ Part-05/00-notes-app/src/components/Note.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import Notes from './Notes';
+import { jest } from '@jest/globals';
 
 test('renders content', () => {
   const note = {
@@ -22,4 +24,21 @@ test('renders content', () => {
   //   );
 
   //   expect(element).toBeDefined();
+});
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    correct: true,
+  };
+
+  const mockHandler = jest.fn();
+
+  render(<Notes note={note} updateNote={mockHandler} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText('✅ Correct');
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
