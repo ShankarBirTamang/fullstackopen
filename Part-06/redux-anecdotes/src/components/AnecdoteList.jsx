eba70ea8715fa230f21bb096bf0f451af9ed970e
ShankarBirTamang <sankar.jt19@gmail.com>
2025-09-13 10:55:38 +0545

    Exercise 6.9 filter with redux complete

--- Part-06/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part-06/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { voteFor } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => state.anecdotes);
+  const filteredValue = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   const vote = (id) => {
@@ -12,9 +13,12 @@ const AnecdoteList = () => {
 
   //sort anecdotes by votes in descending order ( highest votes first)
   const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
+  const filteredAnecdotes = sortedAnecdotes.filter((anecdote) =>
+    anecdote.content.toLowerCase().includes(filteredValue.toLowerCase())
+  );
   return (
     <div>
-      {sortedAnecdotes.map((anecdote) => (
+      {filteredAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
