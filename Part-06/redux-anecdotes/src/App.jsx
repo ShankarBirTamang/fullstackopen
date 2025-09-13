1a89614681bab40535aa668c4ffa69e3f267615c
ShankarBirTamang <sankar.jt19@gmail.com>
2025-09-13 12:18:34 +0545

    Exercise 6.14 getting data from json server

--- Part-06/redux-anecdotes/src/App.jsx
+++ Part-06/redux-anecdotes/src/App.jsx
@@ -2,8 +2,18 @@ import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useEffect } from "react";
+import anecdotesService from "./services/anecdotes";
+import { useDispatch } from "react-redux";
+import { getAllAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdotesService.getAll().then((anecdotes) => {
+      dispatch(getAllAnecdotes(anecdotes));
+    });
+  }, []);
   return (
     <div>
       <h2>Anecdotes</h2>
