6f68722d4ea104ca236a0c44bd4670a5f1bc30b0
Shankar Bir Tamang <sankar.jt19@gmail.com>
2025-09-14 12:28:11 +0545

    Part 6d Using React Query

--- Part-06/redux-notes-app/src/index.jsx
+++ Part-06/redux-notes-app/src/index.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
-import store from "./store";
-import App from "./App";
-import { Provider } from "react-redux";
+import App from "./ReactQueryApp";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+  </QueryClientProvider>
 );
