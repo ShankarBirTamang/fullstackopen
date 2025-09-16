ea3aba6153f452f99186a3ed6a768039356bea77
Shankar Bir Tamang <sankar.jt19@gmail.com>
2025-09-16 18:11:59 +0545

    Part 6d Defining the counter context in a separate file

---
+++ Part-06/redux-counter/src/components/Display.jsx
@@ -0,0 +1,8 @@
+import { useCounterValue } from "../CounterContext";
+
+const Display = () => {
+  const counter = useCounterValue();
+  return <h1>Counter: {counter}</h1>;
+};
+
+export default Display;
--- Part-06/redux-counter/src/main.jsx
+++ Part-06/redux-counter/src/main.jsx
@@ -1,45 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { useReducer, useContext } from "react";
-import CounterContext from "./CounterContext";
+import App from "./App";
+import { CounterContextProvider } from "./CounterContext";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-const counterReducer = (state = 10, action) => {
-  console.log("action", action);
-  console.log("state", state);
-  if (action.type === "INCREMENT") {
-    return state + 1;
-  } else if (action.type === "DECREMENT") {
-    return state - 1;
-  } else if (action.type === "RESET") {
-    return 0;
-  }
-  return state;
-};
-
-const Display = () => {
-  const { counter } = useContext(CounterContext);
-  return <h1>Counter: {counter}</h1>;
-};
-
-const Button = ({ type, label }) => {
-  const { counterDispatch } = useContext(CounterContext);
-  return <button onClick={() => counterDispatch({ type })}>{label}</button>;
-};
-
-const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 10);
-
-  return (
-    <CounterContext.Provider value={{ counter, counterDispatch }}>
-      <div>
-        <Display />
-        <Button type="INCREMENT" label="+" />
-        <Button type="DECREMENT" label="-" />
-        <Button type="RESET" label="reset" />
-      </div>
-    </CounterContext.Provider>
-  );
-};
-root.render(<App />);
+root.render(
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
+);
