bfecf084000dfd7ceafbde6f878994f7f8d6b5e2
Shankar Bir Tamang <sankar.jt19@gmail.com>
2025-09-18 18:22:54 +0545

    Part 7a Add useNavigate to programmatically navigate to a url

--- Part-07/react-router/src/App.jsx
+++ Part-07/react-router/src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
 import Notes from "./components/Notes";
 import Note from "./components/Note";
+import Login from "./components/Login";
 
 const Home = () => (
   <div>
@@ -10,14 +17,14 @@ const Home = () => (
   </div>
 );
 
-const Users = () => (
+const Users = ({ user }) => (
   <div>
     {" "}
-    <h2>Users</h2>{" "}
+    <h2>Users :{user}</h2>{" "}
   </div>
 );
 
-const notes = [
+const notesAtStart = [
   {
     content: "the app state is in redux store",
     important: false,
@@ -46,6 +53,8 @@ const notes = [
 ];
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [notes, setNotes] = useState(notesAtStart);
   const padding = {
     padding: 5,
   };
@@ -62,13 +71,26 @@ const App = () => {
         <Link style={padding} to="/users">
           users
         </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to="/login">
+            login
+          </Link>
+        )}
       </div>
 
       <Routes>
         <Route path="/notes/:id" element={<Note notes={notes} />} />
         <Route path="/notes" element={<Notes notes={notes} />} />
-        <Route path="/users" element={<Users />} />
+        <Route
+          path="/users"
+          element={
+            user ? <Users user={user} /> : <Navigate replace to="/login" />
+          }
+        />
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
       </Routes>
 
       <div>
