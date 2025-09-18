d610efd2ecf670368710b49f886269f6e1f6317e
Shankar Bir Tamang <sankar.jt19@gmail.com>
2025-09-18 17:13:46 +0545

    Part 7a Add parameterized route and useParams to read the parameter

--- Part-07/react-router/src/App.jsx
+++ Part-07/react-router/src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Notes from "./components/Notes";
+import Note from "./components/Note";
 
 const Home = () => (
   <div>
     {" "}
     <h2>TKTL notes app</h2>{" "}
-  </div>
-);
-
-const Notes = () => (
-  <div>
-    {" "}
-    <h2>Notes</h2>{" "}
   </div>
 );
 
@@ -21,6 +16,34 @@ const Users = () => (
     <h2>Users</h2>{" "}
   </div>
 );
+
+const notes = [
+  {
+    content: "the app state is in redux store",
+    important: false,
+    id: "1",
+  },
+  {
+    content: "state changes are made with actions",
+    important: false,
+    id: "2",
+  },
+  {
+    id: "3085",
+    content: "action reaches reducers",
+    important: true,
+  },
+  {
+    id: "7181",
+    content: "action reaches reducers",
+    important: true,
+  },
+  {
+    id: "256f",
+    content: "component dispatches action",
+    important: true,
+  },
+];
 
 const App = () => {
   const padding = {
@@ -42,7 +65,8 @@ const App = () => {
       </div>
 
       <Routes>
-        <Route path="/notes" element={<Notes />} />
+        <Route path="/notes/:id" element={<Note notes={notes} />} />
+        <Route path="/notes" element={<Notes notes={notes} />} />
         <Route path="/users" element={<Users />} />
         <Route path="/" element={<Home />} />
       </Routes>
