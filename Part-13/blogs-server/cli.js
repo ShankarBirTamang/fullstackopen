a74b27a512278aaf04de6a06177e04d6b4e1d977
ShankarBirTamang <sankar.jt19@gmail.com>
2025-11-04 16:56:59 +0545

    Exercise 13.3

--- Part-13/blogs-server/cli.js
+++ Part-13/blogs-server/cli.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize, QueryTypes } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  // logging: false,
   dialectOptions: {
     ssl: {
       require: true,
@@ -13,10 +14,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 const main = async () => {
   try {
     await sequelize.authenticate();
-    const notes = await sequelize.query("SELECT * FROM notes", {
+    const blogs = await sequelize.query("SELECT * FROM blogs", {
       type: QueryTypes.SELECT,
     });
-    console.log(notes);
+    blogs.forEach((blog) => {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
+    });
     sequelize.close();
   } catch (error) {
     console.error("Unable to connect to the database:", error);
